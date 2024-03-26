0bf2e2e7c157874ec352e19d590234b0d066d7c6
Jefferson-tan <56427943+Jefferson-tan@users.noreply.github.com>
2024-03-27 00:52:11 +0700

    Update main.sh
    Added comments and tidied up some of the ASCII arts

--- main.sh
+++ main.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Colour formatting stuff
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
+# ASCII art of Intel, AMD, and Nvidia compressed with gzip and encoded to base64
 intel_logo () {
     printf $BLUE
     base64 -d <<<"H4sIAAAAAAAAA7WWTQ7EIAiF957Co3bRpYtJJukBe5JZ9Ed4DxGaTsOmVHifSsVao8++rWTfpPXYkhLkTItpPZQ/DoFcFAsCxJxYVnYFtVJIA+WUli20b59H+ZMyuZy9MgZCDVWuaZwWTqsHWAkc4WN7qq6weOb5AOkuwntUxWLNyvoxVzMhfjUCElh3qZ5vuB3gGRlDyhT+IbPmkDXxFLnVSsVg6cHJREHfISQzAmKUkQfhAssRXC+Q0qSFaRblbG/zzgvH56XSOYBpWv9ZYFGHxlBuagM4jE2jid/bbEXQ3kxJWG5A7VuvFKKwjDXwSgKtJmgutq5SaBl7wwJvffTQbrZ+PvIckiKze1aTLcK744SV4pe65T4DpgQG0FMLws1Yb94g8ru2DIg0l39zDSl0qR8qJjuK6AsAAA==" | gunzip
@@ -13,7 +15,7 @@ intel_logo () {
 
 amd_logo () {
     printf $RED
-    base64 -d <<<"H4sIAAAAAAAAA52UOxLDMAhEe06ho1JQpHCRygf0SexJxjKCXZRIQ2PA8PhIrfVz7OYF6rF3/pnFuvz02F//ivgA2W7d9ma2D4fXb9idia0WICV5YMQ8CJ8V8TBqUnsTFAw+6jV8W4k+E1IA0d5d2EhLhRBHjaEMqAQN46a4LerGgU2A03T95QG7khluTE0bWnTYUpA8nQI70U13ymj7foKGHalv1hhxwBG6ZmBfVrG56OxR8G49jXsIFw/L04m+6eQEYFU73RkGAAA=" | gunzip
+    base64 -d <<<"H4sIAAAAAAAAA52UMRLEIAhFe07hUSksUqTYygN6kp1MJi7L/1+TOFSC/AeopYzVW43GtguPxsMi1xHnvW1PzWIC4le+GmHTid4+twHqW3ybc0f6iTTiiyJ+jI4CwUWNg//ve+7AqjaFPitA7F6pdtFSE8QwtRzEx+oYxFALZo4VL2ChPenl4E3IYj6qvIWbsrLhzZEBa3mXKmLT1hFg1gYYn7AdxmpEVJA8x9Xmyw8ghA3k8OW9XEpnEJ1y9gWEyEozAwYAAA==" | gunzip
     printf $NC
 }
 
@@ -22,11 +24,13 @@ nvidia_logo () {
     base64 -d <<< "H4sIAAAAAAAAA+WVOw6EMAxEe06Ro7pI6WIlpBwwJ6HBkHjsxEGLtljkCjvMi3+kNHxq+dSyv23bGOLnFLVksSucxUViL1KA+t7oksrPydrzfY0GOboqkZ8QgXqGtIUwrETxdUSJZvt1mALc5JI1Fcrm6Wx/MUSh+DnpieG7PkoTYN1bxGvjAJEGik21SzNpIt25yo2geUI0BPFnDSYIQ+yk9bN0qw8A7Bhro4xmgZt1h3sPUuKozgjsdMnucipB08sudASSQIv2KgtaK+oi8kd/2gNhIHJMdggAAA==" | gunzip
     echo ""
     printf $NC
-    base64 -d <<< "H4sIAAAAAAAAA6WSMQ6AMAhFd0/BUTswdGBw8oCexKSNsf8XpEbzFwt8Xiki7TuPmkk5S3vhPhwZ/HM0sgXfmEc38YIFVaE9+nI0RrWWndgRyFPvgJrwtJyW1CDOnsz66Pm2CuXlKx/UULvymw/x8PKrgNHeWjZsHVQRLF6Sdy0zO9Dpcswq9xsE7CaT6XYBL4SiI+0DAAA=" | gunzip
+    base64 -d <<< "H4sIAAAAAAAAA6XTzwqAIAwH4HtPsUcVkujQoZMQBr2bT1IWxbbf0v7ELuqaH1OJ9i+FvhhdCm3OmtncRHT+vLDpuI3Ha1wv7bdQArNu3rEh4MYUHIteAk2x9hbAR7qTU0hlFSPb0vIO0G8ymYCsKMVy3sZIWFi5h8xR1AAnKv8ypfKjU0MNZvXQffHQ4W7exQu0Uivvy2eVnwNcNN1caPAKoOLBqQ8EAAA=" | gunzip
 }
 
+# Script description
 echo "Simple script to detect GPUs in a system. You need to have lshw installed for it to work."
 
+# This part checks if lshw is installed or not
 if command -- lshw >/dev/null 2>&1; then
     echo -e $GREEN"lshw is installed\n"$NC
 else
@@ -34,26 +38,24 @@ else
     exit 1
 fi
 
+# Lists out GPU in a system, ignores errors about the command running in non-sudo mode, then filters out the vendors
 listGPU=$( (lshw -C video) 2>/dev/null | awk '$1=="vendor:"{$1=""; print}')
 
-#( lshw -C video ) 2>/dev/null | awk '$1=="vendor:"{$1=""; print $0}'
-
+# Logic for checking which GPU exists in system
 if (grep NVIDIA <<< $listGPU) 1>/dev/null ; then
-    echo NVIDIA GPU detected
+    echo -e "${GREEN}NVIDIA${NC} GPU detected"
     nvidia_logo
 elif (grep Intel <<< $listGPU) 1>/dev/null ; then
     echo Intel GPU detected
     intel_logo
-else
-    echo Unknown GPU
 fi
 
 if (grep AMD <<< $listGPU) 1>/dev/null ; then
-    echo AMD GPU detected
+    echo -e "${RED}AMD${NC} GPU detected"
     amd_logo
 elif (grep Intel <<< $listGPU) 1>/dev/null ; then
-    echo Intel GPU detected
+    echo -e "${BLUE}Intel${NC} GPU detected"
     intel_logo
 else
-    echo Unknown GPU
+    echo Unknown or no GPU detected
 fi
